f57bf67cfb1ece6232d8b7ca2efa043af0eae4e9
Tshepo Mokoena <Tshepo@applord.co.za>
2017-06-27 10:47:37 +0200

    Initial Commit

---
+++ ARtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARtest
+//
+//  Created by Tshepo on 2017/06/27.
+//  Copyright © 2017 applord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
