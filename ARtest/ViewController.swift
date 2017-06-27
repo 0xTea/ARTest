866432a93dad976a04fa6c613588c54e1766bddc
Tshepo Mokoena <Tshepo@applord.co.za>
2017-06-27 11:23:15 +0200

    working block ar thing

--- ARtest/ViewController.swift
+++ ARtest/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Create a session configuration
+        let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
+        // Run the view's session
+        sceneView.session.run(configuration)
+         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let cubeNode = SCNNode(geometry: box)
+        cubeNode.position = SCNVector3(0, 0, -0.2) // SceneKit/AR coordinates are in meters
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    }
 }
 
