8bfcf3d770090c2aebb3350eccf726cb4a5e59a3
Tshepo Mokoena <Tshepo@applord.co.za>
2017-06-28 21:23:38 +0200

    adding single node gravity fall on carbon touch

--- ARtest/ViewController.swift
+++ ARtest/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController ,ARSCNViewDelegate {
     var hydro3: SCNNode!
     var hydro4: SCNNode!
     
+    let collisionRollingBall: Int = 1 << 0
+    let collsionTarget: Int = 1 << 1
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -31,13 +33,17 @@ class ViewController: UIViewController ,ARSCNViewDelegate {
         
         // Create a SceneKit plane to visualize the node using its position and extent.
         let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        //adding texture to plane drawing
         var material = SCNMaterial()
         let img = UIImage(named:"tron_grid.png");
         material.diffuse.contents = img;
         plane.materials = [material]
+        
         let planeNode = SCNNode(geometry: plane)
         planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
-       let groundShape = SCNPhysicsShape(geometry: plane, options: nil)
+        let groundShape = SCNPhysicsShape(geometry: plane, options: nil)
+        let groundBody = SCNPhysicsBody(type: .kinematic, shape: groundShape)
+        planeNode.physicsBody = groundBody
         
         // SCNPlanes are vertically oriented in their local coordinate space.
         // Rotate it to match the horizontal orientation of the ARPlaneAnchor.
@@ -69,6 +75,7 @@ class ViewController: UIViewController ,ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         sceneView.delegate = self
+        setupscene()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -79,25 +86,12 @@ class ViewController: UIViewController ,ARSCNViewDelegate {
         if hitResults.count > 0 {
             let result = hitResults[0]
             let node = result.node
+            hydro1.physicsBody?.isAffectedByGravity = true
             if node == carbon
             {
-                let move = SCNAction.moveBy(x: 0.5, y: 0, z:0.5,duration: 2)
-                let action = SCNAction.rotateBy(x: 0, y: 0, z:CGFloat(2*Double.pi), duration: 10)
                 SCNTransaction.begin()
-                SCNTransaction.animationDuration = 9
-                //node.runAction(action)
-                hydro1.runAction(action)
-                hydro1.pivot = SCNMatrix4MakeTranslation(carbon.position.x,carbon.position.y,carbon.position.z)
-                hydro2.pivot = SCNMatrix4MakeTranslation(carbon.position.x,carbon.position.y,carbon.position.z)
-                hydro3.pivot = SCNMatrix4MakeTranslation(carbon.position.x,carbon.position.y,carbon.position.z)
-                hydro4.pivot = SCNMatrix4MakeTranslation(carbon.position.x,carbon.position.y,carbon.position.z)
-                hydro2.runAction(action)
-                hydro3.runAction(action)
-                hydro4.runAction(action)
-                hydro2.runAction(move)
-                hydro3.runAction(move)
-                hydro4.runAction(move)
-                hydro1.runAction(move)
+                hydro1.physicsBody?.isAffectedByGravity = true // dropping of one node at gravitional pull
+                hydro1.physicsBody?.contactTestBitMask = 0 // testing
                 SCNTransaction.commit()
                 
             }
@@ -117,46 +111,65 @@ class ViewController: UIViewController ,ARSCNViewDelegate {
         sceneView.delegate = self
         
         
+        // Create a session configuration
+        let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        
+    }
+    func setupscene()
+    {
         let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
         tapRecognizer.numberOfTapsRequired = 1
         tapRecognizer.numberOfTouchesRequired = 1
         sceneView.gestureRecognizers = [tapRecognizer]
-        // Create a session configuration
-        let configuration = ARWorldTrackingSessionConfiguration()
-        configuration.planeDetection = .horizontal
-        // Run the view's session
         
-         let methaneMolecule = SCNNode()
+        
+        //
+        let methaneMolecule = SCNNode()
+        let carbonshape = SCNPhysicsShape(geometry:  Atoms.carbonAtom(), options: nil)
+        let hydroshape = SCNPhysicsShape(geometry:  Atoms.hydrogenAtom(), options: nil)
+        
+        
+        //defining physics body for spheres
+        let carbonBody = SCNPhysicsBody(type: .dynamic, shape: carbonshape)
+        let hydrobody  = SCNPhysicsBody(type: .dynamic, shape: hydroshape)
+        
+       //testing collison
+        hydrobody.categoryBitMask = 0xFFFFFFFF
+        hydrobody.contactTestBitMask = 0xFFFFFFFF
+        
+        carbonBody.isAffectedByGravity = false
+        hydrobody.isAffectedByGravity = false
         
         // 1 Carbon
-         carbon = nodeWithAtom(atom: Atoms.carbonAtom(), molecule: methaneMolecule, position: SCNVector3Make(0,0,0))
+        carbon = nodeWithAtom(atom: Atoms.carbonAtom(), molecule: methaneMolecule, position: SCNVector3Make(0,0,0))
+        carbon.physicsBody = carbonBody
         
         // 4 Hydrogen
-        hydro1 = nodeWithAtom(atom:Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(-0.4, 0, 0))
-        hydro2 = nodeWithAtom(atom:Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(+0.4, 0, 0))
-        hydro3 = nodeWithAtom(atom :Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(0, -0.4, 0))
-        hydro4 = nodeWithAtom(atom :Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(0, +0.4, 0))
+        hydro1 = nodeWithAtom(atom:Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(-0.04, 0, 0))
+        hydro2 = nodeWithAtom(atom:Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(+0.04, 0, 0))
+        hydro3 = nodeWithAtom(atom :Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(0, -0.04, 0))
+        hydro4 = nodeWithAtom(atom :Atoms.hydrogenAtom(), molecule: methaneMolecule, position: SCNVector3Make(0, +0.04, 0))
         
+        hydro1.physicsBody = hydrobody
         
-        sceneView.session.run(configuration)
-        let sphere = SCNSphere(radius: 0.1)
-        sphere.firstMaterial!.diffuse.contents = UIColor.yellow
         let molecule = SCNText()
         molecule.string = "Methane"
         molecule.font =  UIFont.boldSystemFont(ofSize: 0.1)
-        
         let textNode = SCNNode(geometry: molecule)
-        sphere.firstMaterial!.specular.contents = UIColor.red
         textNode.position = SCNVector3(-0.1, 0.1, -0.6)
         
+        //texture spherical mapping
         methaneMolecule.position = SCNVector3(0, 0, -0.6)
         let background = UIImage(named: "IBLBlurred.png")
+        
         sceneView.scene.background.contents = background;
         sceneView.scene.rootNode.addChildNode(textNode)
         sceneView.scene.rootNode.addChildNode(methaneMolecule)
-        
-        
-        
         
     }
     
